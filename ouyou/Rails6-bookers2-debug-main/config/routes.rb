1cf715117370ead7b56f6f4c5dc29e78b89c3143
EC2 Default User <ec2-user@ip-172-31-12-74.ap-northeast-1.compute.internal>
2022-09-10 15:47:36 +0000

    ouyoukadai6 finish

--- ouyou/Rails6-bookers2-debug-main/config/routes.rb
+++ ouyou/Rails6-bookers2-debug-main/config/routes.rb
@@ -4,17 +4,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
-  get "home/about"=>"homes#about"
+  get "home/about" => "homes#about"
   get "search" => "searches#search"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
-    resource :book_comments, only:[:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
+  
   resources :users, only: [:index,:show,:edit,:update]
   
   resources :users do
-    resource :relationships, only:[:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
     member do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
